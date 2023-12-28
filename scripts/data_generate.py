dd6debec3dd037c171a6b4b37df49ba127b3ae4c
Flantori <zhengyy0621@foxmail.com>
2023-12-28 23:00:12 +0800

    initial push

---
+++ scripts/data_generate.py
@@ -0,0 +1,155 @@
+"""
+此脚本用于生成数据集，每个样本包含多个 gD & gN 数据通道和标签。
+求解区域为 [-1, 1], [-1, 1]。
+"""
+
+from typing import Sequence
+from time import time
+import argparse
+
+import numpy as np
+from numpy.random import rand
+from numpy.typing import NDArray
+from fealpy.cem.generator import LaplaceDataGenerator2d
+import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument("config", help="path of the .yaml file")
+
+
+def levelset(p: NDArray, centers: NDArray, radius: NDArray):
+    """
+    Calculate level set function value.
+    """
+    struct = p.shape[:-1]
+    p = p.reshape(-1, p.shape[-1])
+    dis = np.linalg.norm(p[:, None, :] - centers[None, :, :], axis=-1) # (N, NCir)
+    ret = np.min(dis - radius[None, :], axis=-1) # (N, )
+    return ret.reshape(struct)
+
+
+args = parser.parse_args()
+with open(args.config, "r") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
+
+EXT = config['data']['ext']
+SIGMA = config['data']['sigma']
+FREQ = config['data']['freq']
+PHR = config['data']['phrase']
+OMEGA_NAME = config['data']['ch_names']
+assert len(OMEGA_NAME) == len(FREQ) * len(PHR)
+DTYPE = config['data']['dtype']
+output_folder = config['output_folder']
+
+if output_folder[-1] != "/":
+    output_folder += "/"
+
+def main(sigma_iterable: Sequence[int], seed=0):
+    np.random.seed(seed)
+
+    for sigma_idx in sigma_iterable:
+
+        count = 0
+
+        while count < 100:
+
+            ctrs = rand(3, 2) * 1.6 - 0.8 # (NCir, GD)
+            b = np.min(0.9-np.abs(ctrs), axis=-1) # (NCir, )
+            rads = rand(3) * (b-0.1) + 0.1 # (NCir, )
+            ls_fn = lambda p: levelset(p, ctrs, rads)
+
+            generator = LaplaceDataGenerator2d.from_cos(
+                box=[-1, 1, -1, 1],
+                nx=EXT, ny=EXT,
+                levelset=ls_fn,
+                sigma_vals=SIGMA,
+                freq=FREQ,
+                phrase=PHR
+            )
+            count += 1
+
+            if generator.is_available():
+                count = 100
+                generator.save(sigma_idx,
+                            output_folder,
+                            OMEGA_NAME,
+                            dtype=np.dtype(DTYPE))
+
+
+def estimate_space_occupied(n_float: int, n_bool: int, n_samples: int, dtype: str):
+    if dtype == "float32":
+        single = n_float * 4 + n_bool
+    elif dtype == "float64":
+        single = n_float * 8 + n_bool
+    else:
+        raise ValueError(f"Unsupported dtype '{DTYPE}'.")
+    return n_samples * single
+
+
+def _unit(bytes: int):
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(np.floor(np.log(bytes) / np.log(1024)))
+    if i == 0:
+        return f"{bytes:.2f} {size_name[i]}"
+    else:
+        return f"{bytes / 1024 ** i:.2f} {size_name[i]}"
+
+
+if __name__ == "__main__":
+    import os
+
+    process_num = config['process_num']
+    space_occ = estimate_space_occupied(EXT * 8 * len(OMEGA_NAME),
+                                        (EXT+1)**2,
+                                        config['tail'] - config['head'],
+                                        DTYPE)
+
+    print("Start generating data...")
+    print(f"using {process_num} processes")
+    print("Config:")
+    print(f"    mesh: {EXT}x{EXT}")
+    print(f"    sigma(inclusion): {SIGMA[0]}")
+    print(f"    sigma(background): {SIGMA[1]}")
+    print(f"    freq: {FREQ}")
+    print(f"    phrase: {PHR}")
+    print(f"    dtype: {DTYPE}")
+    print("Output:")
+    print(f"    data shape: {2}x{EXT*4}")
+    print(f"    n_channel: {len(OMEGA_NAME)}")
+    print(f"    label shape: {EXT+1}x{EXT+1}")
+    print(f"    data index range: {config['head']}~{config['tail']}")
+    print(f"Space occupation estimate: {_unit(space_occ)},")
+    print(f"will be saved to folder: {output_folder}", end='\n\n')
+    signal_ = input("Continue? (y/n)")
+
+    if signal_ not in {'y', 'Y'}:
+        print("Aborted.")
+        exit(0)
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    from multiprocessing import Pool
+    from fealpy.ml import timer
+
+    pool = Pool(process_num)
+    tmr = timer()
+    tmr.send(None)
+
+    NUM = tuple(range(config['head'], config['tail']))
+
+    PART = 4
+    TM = int(time())
+
+    pool.apply_async(main, (NUM[0::PART], 621 + TM))
+    pool.apply_async(main, (NUM[1::PART], 928 + TM))
+    pool.apply_async(main, (NUM[2::PART], 122 + TM))
+    pool.apply_async(main, (NUM[3::PART], 222 + TM))
+
+    pool.close()
+    pool.join()
+
+    tmr.send('stop')
+
+    print("Done.")
---
+++ scripts/laplace_beltrami.py
@@ -0,0 +1,95 @@
+"""
+此脚本用于生成求解区域的边界 LB 算子的特征值和特征向量，并存储为单个 .npz 文件。
+"""
+
+from typing import Tuple
+import argparse
+
+import yaml
+import numpy as np
+from numpy.typing import NDArray
+from scipy.linalg import eigh
+from fealpy.functionspace import LagrangeFESpace
+from fealpy.fem import (
+    BilinearForm,
+    ScalarDiffusionIntegrator,
+    ScalarMassIntegrator
+)
+from fealpy.mesh import IntervalMesh
+from fealpy.mesh import UniformMesh2d
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("config", type=str, help="config file")
+parser.add_argument("--plot", help="plot the eigenvalues", action="store_true")
+args = parser.parse_args()
+
+with open(args.config, "r") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
+Q_ = config['fem']['integral']
+assert isinstance(Q_, int)
+assert Q_ >= 1
+
+def laplace_eigen_fem(mesh) -> Tuple[NDArray, NDArray, NDArray, NDArray]:
+    """
+    @brief
+    """
+    space = LagrangeFESpace(mesh, p=1)
+    bform_0 = BilinearForm(space)
+    bform_0.add_domain_integrator(ScalarDiffusionIntegrator(q=Q_))
+    A = bform_0.assembly().toarray()
+    bform_1 = BilinearForm(space)
+    bform_1.add_domain_integrator(ScalarMassIntegrator(q=Q_))
+    M = bform_1.assembly().toarray()
+    w, v = eigh(A, M)
+    return w, v, A, M
+
+EXTx, EXTy = config['mesh']["ext"]
+Lx, Ly = config['mesh']['length']
+Hx = EXTx/Lx
+Hy = EXTy/Ly
+Origin = config['mesh']['origin']
+N_REFINE = config['mesh']['refine']
+
+
+print("Generating Boundary Laplace Beltrami operator...")
+print(f"Config:")
+print(f"  - Domain: [{Origin[0]}, {Origin[0] + Lx}]x[{Origin[1]}, {Origin[1] + Ly}]")
+print(f"  - Mesh: {EXTx}x{EXTy}, refinement: {N_REFINE}")
+print(f"  - Integral points: {Q_}")
+print(f"will be saved to file: {config['file']}", end='\n\n')
+signal_ = input("Continue? (y/n) ")
+
+
+if signal_ in {'y', 'Y'}:
+
+    uniform_mesh = UniformMesh2d([0, EXTx, 0, EXTy], [Hx, Hy], origin=Origin)
+    mesh = IntervalMesh.from_mesh_boundary(uniform_mesh)
+    NN = mesh.number_of_nodes()
+    mesh.uniform_refine(N_REFINE)
+
+    w, v, A, M = laplace_eigen_fem(mesh)
+
+    L = w.shape[0]
+
+    w = w[1:NN+1]
+    v = v[1:NN+1, :NN]
+    A = A[:NN, :NN]
+    M = M[:NN, :NN]
+
+    np.savez(config['file'], w=w, v=v, A=A, M=M)
+    print("Saved.")
+
+    if args.plot:
+        from matplotlib import pyplot as plt
+
+        fig = plt.figure()
+        axes = fig.add_subplot()
+        f = np.arange(0, NN)
+        axes.plot(f, np.sqrt(w))
+        axes.plot(f, (f+1)/2*np.pi/4)
+        plt.show()
+
+else:
+    print("Aborted.")

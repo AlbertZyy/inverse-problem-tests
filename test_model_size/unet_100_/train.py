5f6520a81a08d4dd2eedfa1d31f927d923df6a06
Flantori <zhengyy0621@foxmail.com>
2023-12-29 15:34:19 +0800

    update the training script

--- test_model_size/unet_100_/train.py
+++ test_model_size/unet_100_/train.py
@@ -1,8 +1,10 @@
 
 import sys
+import argparse
 
 sys.path.append('./src')
 
+import yaml
 import torch
 from torch.optim import SGD
 from torch.utils.data import DataLoader
@@ -10,26 +12,99 @@ from tensorboardX import SummaryWriter
 
 from unet_100 import build_model
 from dataset import NPZDataset
+from common import loss_fn
 
-SAVE = True
-GPU_ID = 3
+
+### parse args
+
+parser = argparse.ArgumentParser()
+parser.add_argument('config', type=str, help='config file.')
+
+args = parser.parse_args()
+
+with open(args.config, 'r') as file:
+    config = yaml.load(file, Loader=yaml.FullLoader)
+
+SAVE = config['save']
+GPU_ID = config['gpu_id']
 device = torch.device(f'cuda:{GPU_ID}' if torch.cuda.is_available() else 'cpu')
 
+epoch_start = config['epoch_start']
+epoch_end = config['epoch_end']
+n_epoch = epoch_end - epoch_start
 
-def loss_fn(y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
-    val = - torch.log(y_pred) * y_true - torch.log(1 - y_pred) * (1 - y_true)
-    return torch.mean(val)
+lr = config['lr']
+momentum = config['momentum']
+weight_decay = config['weight_decay']
 
-model, MODEL_NAME = build_model(device, s=0.0, s_grad=True)
+### build model & data set
 
-optim = SGD(model.parameters(), lr=1e-3, momentum=0.9, weight_decay=1e-9)
-train_dataset = NPZDataset("./data/gdgn_64_64_train/", 2000)
-validate_dataset = NPZDataset("./data/gdgn_64_64_validate/", 100)
-loader = DataLoader(train_dataset, batch_size=20, shuffle=True)
-loader_2 = DataLoader(validate_dataset, batch_size=20, shuffle=True)
+model, MODEL_NAME = build_model(device, s=config['s'], s_grad=config['s_grad'])
 
-writer_1 = SummaryWriter(f'./test_model_size/unet_100_/log_{MODEL_NAME}', flush_secs=30)
+data_conf = config['data']
+train_dataset = NPZDataset(data_conf['train_set_location'], data_conf['train_set_volume'])
+validate_dataset = NPZDataset(data_conf['validate_set_location'], data_conf['validate_set_volume'])
+loader = DataLoader(train_dataset, batch_size=data_conf['train_batch_size'], shuffle=True)
+loader_2 = DataLoader(validate_dataset, batch_size=data_conf['validate_batch_size'], shuffle=True)
 
+iter_per_epoch, remander = divmod(len(train_dataset), data_conf['train_batch_size'])
+assert remander == 0
+
+### confirm
+
+print(f'\nStart training {MODEL_NAME} on {device}...')
+print(f"s initialized to {config['s']}", end='')
+
+if config['s_grad']:
+    print(', and will be trained.')
+else:
+    print(", and fixed.")
+
+print(f'Total {n_epoch} epochs(start with {epoch_start}), {iter_per_epoch} iterations per epoch.')
+print(f'Training set size: {len(train_dataset)}.')
+print(f'Validation set size: {len(validate_dataset)}.', end='\n\n')
+print("Train(SGD) setup:")
+print(f"  - learning rate: {config['lr']}")
+print(f"  - momentum: {config['momentum']}")
+print(f"  - weight decay: {config['weight_decay']}", end='\n\n')
+
+log_dir = config['log_dir']
+if log_dir[-1] != '/':
+    log_dir += '/'
+
+print(f"Logs will be saved in {log_dir}")
+
+checkpoint_path = ''
+
+if SAVE:
+    checkpoint_dir = config['checkpoint_dir']
+
+    if checkpoint_dir[-1]!= '/':
+        checkpoint_dir += '/'
+
+    checkpoint_path = checkpoint_dir + MODEL_NAME + '.pth'
+    print(f"Checkpoints will be saved as {checkpoint_path}", end='\n\n')
+
+else:
+    print("Checkpoints saving disabled.", end='\n\n')
+
+signal_ = input("Continue? (y/n)")
+
+if signal_ not in {'y', 'Y'}:
+    print("Aborted.")
+    exit(0)
+
+### train
+
+optim = SGD(model.parameters(), lr=lr,
+            momentum=momentum, weight_decay=weight_decay)
+
+writer_1 = SummaryWriter(log_dir + MODEL_NAME, flush_secs=30)
+
+if SAVE:
+    import os
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
 
 def train(epoch: int):
     step = 0
@@ -42,12 +117,12 @@ def train(epoch: int):
         loss.backward()
         optim.step()
         step += 1
-        print(f"epoch: {epoch}, step: {step}, loss: {loss.item()}")
-        writer_1.add_scalar('loss(train)', loss.item(), epoch*100 + step)
-        writer_1.add_scalar('s', model.df_solver._frac.s.item(), epoch*100 + step)
+
+        writer_1.add_scalar('loss(train)', loss.item(), epoch*iter_per_epoch + step)
+        writer_1.add_scalar('s', model.df_solver._frac.s.item(), epoch*iter_per_epoch + step)
 
     if SAVE:
-        torch.save(model.state_dict(), f"./test_model_size/unet_100_/{MODEL_NAME}.pth")
+        torch.save(model.state_dict(), checkpoint_path)
 
 
 def validate(epoch):
@@ -55,11 +130,11 @@ def validate(epoch):
     with torch.no_grad():
         y_out = model(gdgn.to(device=device))
         loss = loss_fn(y_out, label.flatten().to(dtype=torch.float32, device=device))
-        print(f"epoch: {epoch}, loss: {loss.item()}")
-    writer_1.add_scalar('loss(validate)', loss.item(), epoch*100+100)
+
+    writer_1.add_scalar('loss(validate)', loss.item(), (epoch + 1)*iter_per_epoch)
 
 
-for epoch in range(10):
+for epoch in range(epoch_start, epoch_end):
     train(epoch)
     validate(epoch)
 

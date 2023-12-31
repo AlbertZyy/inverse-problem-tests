d62b3bbd025bbcbacaec7c15334db82fb182940f
AlbertZyy <zhengyy0621@foxmail.com>
2023-12-31 14:06:18 +0800

    add plot confs

--- scripts/scalar_plot.py
+++ scripts/scalar_plot.py
@@ -1,9 +1,32 @@
 
 import argparse
+from typing import Dict, Any
 
 import numpy as np
+from numpy.typing import NDArray
 import matplotlib.pyplot as plt
 import yaml
+
+
+def exponential_moving_average(time_series: NDArray, alpha: float):
+    """
+    Calculate the exponential moving average (EMA) of a time series.
+
+    Parameters:
+    - time_series (numpy.ndarray): Input time series data.
+    - alpha (float): Smoothing factor, between 0 and 1.
+
+    Returns:
+    - numpy.ndarray: Exponential moving average of the input time series.
+    """
+
+    ema = np.zeros_like(time_series)
+    ema[0] = time_series[0]
+
+    for i in range(1, len(time_series)):
+        ema[i] = (1 - alpha) * time_series[i] + alpha * ema[i - 1]
+
+    return ema
 
 
 def set_lim(conf: str):
@@ -24,7 +47,7 @@ args = parser.parse_args()
 
 
 with open(args.input_file, 'r') as f:
-    config = yaml.load(f.read(), Loader=yaml.FullLoader)
+    config: Dict[str, Any] = yaml.load(f.read(), Loader=yaml.FullLoader)
 
 
 fig = plt.figure(config['fig_name'])
@@ -37,8 +60,11 @@ for file_name in config['csv_files']:
     step = data[:, 1].astype(np.int_)
     value = data[:, 2]
 
-    if config['log']:
+    if config.get('log', False):
         value = np.log10(value)
+
+    if config.get('smooth', 0):
+        value = exponential_moving_average(value, config['smooth'])
 
     if config['plot_type'] in {'line', 'Line', 'L'}:
         axes.plot(step, value)
@@ -56,9 +82,9 @@ axes.set_ylabel(config['ylabel'])
 axes.set_title(config['title'])
 axes.grid()
 
-lim_args, lim_kwargs = set_lim(config['xlim'])
+lim_args, lim_kwargs = set_lim(config.get('xlim', 'auto'))
 axes.set_xlim(*lim_args, **lim_kwargs)
-lim_args, lim_kwargs = set_lim(config['ylim'])
+lim_args, lim_kwargs = set_lim(config.get('ylim', 'auto'))
 axes.set_ylim(*lim_args, **lim_kwargs)
 
 if config['save']:

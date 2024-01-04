5160c246b022cfdac945ea59c13bff2c511d759b
AlbertZyy <zhengyy0621@foxmail.com>
2024-01-04 18:53:28 +0800

    add multi-channel data feature

--- src/data_feature.py
+++ src/data_feature.py
@@ -5,7 +5,7 @@ from torch import Tensor
 from torch.nn import Module
 
 from fdm import LaplaceFDMSolver
-from fractional import Fractional
+from fractional import Fractional, MultiChannelFractional
 
 
 class DataFeatureFDMSolver(Module):
@@ -47,3 +47,36 @@ class DataFeatureFDMSolver(Module):
         val = self._vmapped(data) # (N*C, NN)
         MESH = self._solver.indexing.shape
         return val.reshape(BATCH_CHANNEL + MESH) # (N, C, Nx, Ny)
+
+
+class MultiChannelDataFeature(Module):
+    def __init__(self, laplace_solver: LaplaceFDMSolver, fractional_lb: MultiChannelFractional) -> None:
+        super().__init__()
+        self._solver = laplace_solver
+        self._frac = fractional_lb
+
+    def solve_phi(self, gd_gn: Tensor) -> Tensor:
+        """
+        @brief From gd_gn data to data feature.
+
+        @param gd_gn: Tensor in shape (C, 2, NN_bd), where NN_bd is the number of\
+               nodes on the boundary.
+
+        @return: Tensor in shape (C, NN).
+        """
+        assert gd_gn.ndim == 3
+        assert gd_gn.shape[1] == 2
+        gd, gn = gd_gn[:, 0, :], gd_gn[:, 1, :]
+        vuh = self._solver.solve_from_gd(gd) # [C, NN]
+        vn = self._solver.normal_derivative(vuh) # [C, NN_bd]
+        gnvn = self._frac(gn - vn) # [C, NN_bd]
+        return self._solver.solve_from_gn(gnvn) # [C, NN]
+
+    def forward(self, data: Tensor):
+        assert data.ndim == 4
+        BATCH_CHANNEL = data.shape[0:2]
+        GDGN = data.shape[2:]
+        data = data.reshape(-1, *GDGN)
+        val = self.solve_phi(data)
+        MESH = self._solver.indexing.shape
+        return val.reshape(BATCH_CHANNEL + MESH)

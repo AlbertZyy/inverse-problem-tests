429da36cbc6ca218feb984b499a56c7ef3e580bb
AlbertZyy <zhengyy0621@foxmail.com>
2024-01-14 20:02:51 +0800

    update

--- test/test_fractional.py
+++ test/test_fractional.py
@@ -19,7 +19,7 @@ frac.from_npz(r"./data/laplace_beltrami_63_63.npz")
 
 # 20% - 391.72
 # 50% - 2448.27
-frac.initialize(s=1.0, hc=10000)
+frac.initialize(s=1.0, hc=1e+5)
 
 dataset = NPZDataset(r"./data/gdgn_64_64_train", 100)
 gd = dataset[81][0][:, 0, :]
@@ -44,26 +44,29 @@ from matplotlib import pyplot as plt
 PI = torch.pi
 
 length = alpha.shape[1] // 2
-x = torch.arange(length, dtype=torch.float32) * PI/4
-x = x.log10_()
+x = torch.arange(1, length+1, dtype=torch.float32) * PI/4
 
 fig = plt.figure()
 
 axes = fig.add_subplot(1, 2, 1)
-axes.plot(x, torch.log10(sum_energy(alpha).T.detach()))
+axes.plot(x, sum_energy(alpha).T.detach())
 axes.grid(True)
-axes.set_ylim(-9., 3.)
 axes.set_xlabel('Frequency')
+axes.set_xscale('log')
+axes.set_ylim(1e-6, 1e+2)
 axes.set_ylabel('Energy')
+axes.set_yscale('log')
 axes.set_title('Original Spectrum')
 axes.legend([1, 2, 3, 4, 5, 6, 8, 16])
 
 axes = fig.add_subplot(1, 2, 2)
-axes.plot(x, torch.log10(sum_energy(alpha_2).T.detach()))
+axes.plot(x, sum_energy(alpha_2).T.detach())
 axes.grid(True)
-axes.set_ylim(-9, 3.)
 axes.set_xlabel('Frequency')
+axes.set_xscale('log')
+axes.set_ylim(1e-6, 1e+2)
 axes.set_ylabel('Energy')
+axes.set_yscale('log')
 axes.set_title('Reconstructed Spectrum')
 
 plt.show()

fcfdbecc810e9f13a51951cbd49400a0c60dd430
Flantori <zhengyy0621@foxmail.com>
2024-01-18 19:42:36 +0800

    add data transform

--- src/dataset.py
+++ src/dataset.py
@@ -1,10 +1,14 @@
 
-from typing import List, Optional, Tuple, Dict, Any
+from typing import List, Optional, Tuple, Dict, Any, Callable
 
 import numpy as np
+from numpy.typing import NDArray
 import torch
 from torch import Tensor
 from torch.utils.data import Dataset
+
+
+ArrayFunction = Callable[[NDArray[Any]], NDArray[Any]]
 
 
 class NPZDataset(Dataset):
@@ -37,10 +41,10 @@ class NPZDataset(Dataset):
         self.use_cache = use_cache
         self._cache: List[Optional[Tuple[Tensor, Tensor]]] = [None, ] * num
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.num
 
-    def __getitem__(self, index):
+    def __getitem__(self, index) -> Tuple[Tensor, Tensor]:
         if self.use_cache and self._cache[index] is not None:
             return self._cache[index]
         else:
@@ -53,6 +57,24 @@ class NPZDataset(Dataset):
             if self.use_cache:
                 self._cache[index] = pair
             return pair
+
+    def transform_to(self, func: ArrayFunction, destination_folder: str):
+        """
+        @brief Transform the data and save to a new folder.
+        """
+        if destination_folder[-1] != '/':
+            destination_folder += '/'
+
+        import os
+        os.makedirs(destination_folder, exist_ok=True)
+
+        for i in range(self.num):
+            datadict = dict(np.load(self.path + f"{i}.npz"))
+            for key, value in datadict.items():
+                if key == self.label_key:
+                    continue
+                datadict[key] = func(value)
+            np.savez(destination_folder + f"{i}.npz", **datadict)
 
 
 def load_npz_dataset(config_dict: Dict[str, Any]) -> NPZDataset:

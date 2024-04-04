e3fae7547b637db3894a18cd8872712f9364466f
AlbertZyy <zhengyy0621@foxmail.com>
2024-04-04 22:31:51 +0800

    update

---
+++ test_denoised/cnn.py
@@ -0,0 +1,64 @@
+
+import torch
+import torch.nn as nn
+from torch import device, float64
+
+
+class NoiseExtractCNN(nn.Module):
+    def __init__(self, n_channels: int, hidden: int):
+        super(NoiseExtractCNN, self).__init__()
+        NC = n_channels
+        NHC = hidden
+        kwargs = dict(padding_mode='circular', dtype=float64)
+        self.conv1 = nn.Conv1d(NC, NHC, 9, padding=4, **kwargs)
+        self.conv2 = nn.Conv1d(NHC, NHC, 3, padding=1, **kwargs)
+        self.bn12 = nn.BatchNorm1d(NHC, momentum=0.9, dtype=float64)
+        self.down = nn.AvgPool1d(kernel_size=2)
+
+        self.btm = nn.Conv1d(NHC, NHC, 3, padding=1, **kwargs)
+
+        self.up = nn.ConvTranspose1d(NHC, NHC, 3, 2, 1, 1, dtype=float64)
+        self.conv3 = nn.Conv1d(NHC, NHC, 3, padding=1, **kwargs)
+        self.conv4 = nn.Conv1d(NHC, NC, 9, padding=4, **kwargs)
+        self.bn34 = nn.BatchNorm1d(NC, momentum=0.9, dtype=float64)
+
+    def forward(self, input):
+        x = self.conv1(input)
+        x = self.conv2(x)
+        x = torch.relu_(self.bn12(x))
+        x = self.down(x)
+
+        x = self.btm(x)
+
+        x = self.up(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = torch.relu_(self.bn34(x))
+        return input + x
+
+
+def build_model(device: device, tag: str):
+
+    model = NoiseExtractCNN(8*2, 128)
+    model.to(device)
+
+    NAME = "cnn"
+
+    FULL_NAME = (NAME + '_' + tag) if tag else NAME
+
+    print(f"Model built: {FULL_NAME}, in device: {device}")
+
+    n_p = sum(p.numel() for p in model.parameters())
+    print(f"Number of unet parameters: {n_p/1e6:.2f}M")
+
+    try:
+        model.load_state_dict(torch.load(f"./test_denoised/ckpts/{FULL_NAME}.pth", map_location=device))
+        print(f"Checkpoint loaded.")
+    except FileNotFoundError:
+        print(f"No checkpoint found.")
+
+    return model, FULL_NAME
+
+
+if __name__ == "__main__":
+    build_model('cpu', '')
---
+++ test_denoised/train.py
@@ -0,0 +1,197 @@
+
+import sys
+import argparse
+
+sys.path.append('./src')
+
+import yaml
+import torch
+from torch.optim import SGD
+from tensorboardX import SummaryWriter
+from tqdm import tqdm, trange
+
+from fractional import Fractional
+from unet_100 import build_model
+from cnn import build_model as build_model_cnn
+from dataset import TPZDataset
+from common import loss_fn
+
+
+### parse args
+
+parser = argparse.ArgumentParser()
+parser.add_argument('config', type=str, help='config file.')
+
+args = parser.parse_args()
+
+with open(args.config, 'r') as file:
+    config = yaml.load(file, Loader=yaml.FullLoader)
+
+tag: str        = config.get('tag', '')
+data_conf       = config['data']
+SAVE            = config['save']
+GPU_ID          = config['gpu_id']
+iter_head: int  = config['iter_head']
+n_epoch: int    = config['epochs']
+lr              = config['lr']
+momentum               = config.get('momentum', 0)
+weight_decay           = config.get('weight_decay', 0.0)
+NOISE: float           = config.get('noise', 0.0)
+use_noise_filter: bool = config.get('use_noise_filter', False)
+type_: bool            = config.get('type_', '')
+
+device = torch.device(f'cuda:{GPU_ID}' if torch.cuda.is_available() else 'cpu')
+
+
+### build model & data set
+
+model, MODEL_NAME = build_model(device, tag=config['tag'], type_=type_)
+denoiser, _  = build_model_cnn(device, tag='C')
+denoiser.eval()
+denoiser.requires_grad_(False)
+
+data_conf = config['data']
+# train_dataset = NPZDataset(data_conf['train_set_location'], data_conf['train_set_volume'], use_cache=True)
+# validate_dataset = NPZDataset(data_conf['validate_set_location'], data_conf['validate_set_volume'], use_cache=True)
+# loader = DataLoader(train_dataset, batch_size=data_conf['train_batch_size'], shuffle=True, num_workers=4, pin_memory=True)
+# loader_2 = DataLoader(validate_dataset, batch_size=data_conf['validate_batch_size'], shuffle=True, num_workers=1, pin_memory=True)
+
+train_dataset = TPZDataset(
+    data_conf['train_set_location'],
+    names=data_conf['train_set_volume'],
+    channel_keys=['1', '2', '3', '4', '5', '6', '8', '16'],
+    num_workers=4,
+    device=device,
+    tqdm=True
+)
+
+validate_dataset = TPZDataset(
+    data_conf['validate_set_location'],
+    names=data_conf['validate_set_volume'],
+    channel_keys=['1', '2', '3', '4', '5', '6', '8', '16'],
+    num_workers=4,
+    device=device,
+    tqdm=True
+)
+
+iter_per_epoch, remander = divmod(len(train_dataset), data_conf['train_batch_size'])
+assert remander == 0
+
+if use_noise_filter:
+    noise_filter = Fractional(252, device=device)
+    noise_filter.from_npz(r"./data/laplace_beltrami_63_63.npz")
+    noise_filter.initialize(s=-0.75)
+    noise_filter.s.requires_grad_(False)
+else:
+    noise_filter = None
+
+### confirm
+
+print(f'\nStart training {MODEL_NAME} on {device}...')
+print(f"  - type: {type_}")
+
+print(f'Total {n_epoch} epochs(iter from {iter_head}), {iter_per_epoch} iterations per epoch.')
+print(f'Training set size: {len(train_dataset)}.')
+print(f'Validation set size: {len(validate_dataset)}.', end='\n\n')
+print(f"NOISE: {NOISE}, filter: {use_noise_filter}.")
+print("Train(SGD) setup:")
+print(f"  - learning rate: {lr}")
+print(f"  - momentum: {momentum}")
+print(f"  - weight decay: {weight_decay}", end='\n\n')
+
+log_dir = config['log_dir']
+if log_dir[-1] != '/':
+    log_dir += '/'
+
+print(f"Logs will be saved in {log_dir}")
+
+checkpoint_path = ''
+
+if SAVE:
+    checkpoint_dir = config['checkpoint_dir']
+
+    if checkpoint_dir[-1] != '/':
+        checkpoint_dir += '/'
+
+    checkpoint_path = checkpoint_dir + MODEL_NAME + '.pth'
+    print(f"Checkpoints will be saved as {checkpoint_path}", end='\n\n')
+
+else:
+    print("Checkpoints saving disabled.", end='\n\n')
+
+signal_ = input("Continue? (y/n)")
+
+if signal_ not in {'y', 'Y'}:
+    print("Aborted.")
+    exit(0)
+
+### train
+
+optim = SGD(model.parameters(), lr=lr,
+            momentum=momentum, weight_decay=weight_decay)
+
+writer_1 = SummaryWriter(log_dir + MODEL_NAME, flush_secs=30)
+
+if SAVE:
+    import os
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+
+def train(epoch: int):
+    step = 0
+
+    for gdgn, label in tqdm(train_dataset.loader(data_conf['train_batch_size']),
+                            desc=f'Epoch {epoch + 1}/{n_epoch}', unit='batch', leave=False):
+        optim.zero_grad()
+
+        noise = torch.randn_like(gdgn[:, :, 0, :]) * NOISE
+        if noise_filter:
+            noise = noise_filter(noise)
+        noise = gdgn[:, :, 0, :] * noise
+        gdgn[:, :, 0, :] += noise
+        gdgn = denoiser(gdgn.reshape(-1, 8*2, 252)).reshape(-1, 8, 2, 252)
+
+        y_out = model(gdgn) # (N, 1, Nx, Ny)
+        loss = loss_fn(y_out, label.flatten().to(dtype=torch.float32, device=device))
+        loss.backward()
+        optim.step()
+        step += 1
+
+    writer_1.add_scalar('loss(train)', loss.item(),
+                        iter_head + (epoch+1)*iter_per_epoch)
+
+    if type_ != 'sng':
+        if type_ != 'single':
+            for i in range(0, 8):
+                writer_1.add_scalar(f's{i}', model.df_solver._frac.s[i].item(),
+                                    iter_head + (epoch+1)*iter_per_epoch)
+        else:
+            writer_1.add_scalar('s', model.df_solver._frac.s.item(),
+                                iter_head + (epoch+1)*iter_per_epoch)
+
+    if SAVE:
+        torch.save(model.state_dict(), checkpoint_path)
+
+
+def validate(epoch):
+    gdgn, label = next(validate_dataset.loader(data_conf['validate_batch_size']))
+    with torch.no_grad():
+
+        noise = torch.randn_like(gdgn[:, :, 0, :]) * NOISE
+        if noise_filter:
+            noise = noise_filter(noise)
+        noise = gdgn[:, :, 0, :] * noise
+        gdgn[:, :, 0, :] += noise
+
+        y_out = model(gdgn.to(device=device))
+        loss = loss_fn(y_out, label.flatten().to(dtype=torch.float32, device=device))
+
+    writer_1.add_scalar('loss(validate)', loss.item(), iter_head + (epoch + 1)*iter_per_epoch)
+
+
+for epoch in trange(0, n_epoch, desc='Training', unit='epoch'):
+    train(epoch)
+    validate(epoch)
+
+writer_1.close()
+print("Done.")

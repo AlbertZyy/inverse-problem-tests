05494b504d3e4556e517b5ee2651a8c368aa753e
Flantori <zhengyy0621@foxmail.com>
2024-07-04 23:59:05 +0800

    feat(fem_df): implement fractional FEM solver and optimize initialization
    Introduce a fractional component to the FEM solver in the test_fem_df.py module.
    This change improves the solver's performance by utilizing the Fractional class
    for more efficient calculations. Additionally, the initialization process for
    the fractional component has been optimized, and the solver's training loop
    has been updated to monitor the loss function more effectively.

--- test/test_fem_df.py
+++ test/test_fem_df.py
@@ -5,38 +5,48 @@ sys.path.append("./src")
 
 import numpy as np
 import torch
-from torch.utils.data import RandomSampler, BatchSampler
+from torch.nn import MSELoss
+from torch.optim import Adam
 from fealpy.torch.mesh import TriangleMesh
-from tqdm import tqdm
 
+from fractional import Fractional
 from fem import DataFeatureFEMSolver, EITDataPreprocessor, LaplaceFEMSolver
-from dataset import NPYDataset
 
 
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 EXT = 63
 
+frac = Fractional(252, device=DEVICE)
+frac.from_npz(r"data/laplace_beltrami_63_63_torch.npz")
+frac.initialize(-0.87)
+frac.requires_grad_(False)
+
 mesh = TriangleMesh.from_box([-1, 1, -1, 1], nx=EXT, ny=EXT, device=DEVICE)
 solver = LaplaceFEMSolver(mesh, p=1, reserve_matrix=True)
 df_prepro = EITDataPreprocessor(solver)
-df_solver = DataFeatureFEMSolver(solver)
-dataset = NPYDataset('data/cir3_e64_64_c8/gd', names=[str(i) for i in range(200)])
-gn_ = torch.from_numpy(np.load('data/cir3_e64_64_c8/gn.npy'))
+df_solver = DataFeatureFEMSolver(solver, frac)
+# dataset = NPYDataset('data/cir3_e64_64_c8/gd', names=[str(i) for i in range(200)])
+gd = torch.from_numpy(np.load('data/cir3_e64_64_c8/gd/50.npy'))
+gn = torch.from_numpy(np.load('data/cir3_e64_64_c8/gn.npy'))
 
-sampler = RandomSampler(dataset)
-batch_sampler = BatchSampler(sampler, batch_size=100, drop_last=False)
+gn = gn.broadcast_to(gd.shape)
+data = torch.stack([gd, gn], dim=-2).to(DEVICE)
+gnvn = df_prepro(data[None, ...])
+target_phi = df_solver(gnvn).detach()
 
-vuh_res = []
-phi_res = []
+frac.initialize(0.0)
+frac.requires_grad_(True)
+criterion = MSELoss()
+optim = Adam((frac.s,), lr=1e-2)
 
-for indices in tqdm(batch_sampler):
-    gd = dataset.__getitems__(indices)
-    gn = gn_.unsqueeze(0).broadcast_to(gd.shape)
-    data = torch.stack([gd, gn], dim=-2).to(DEVICE)
-    gnvn = df_prepro(data)
-    vuh_res.append(solver.residual_fd(df_prepro.vuh).item())
+
+for i in range(0, 200):
+    optim.zero_grad()
     phi = df_solver(gnvn)
-    phi_res.append(solver.residual_fn(df_solver.img).item())
+    loss = (phi - target_phi).pow(2).sum().sqrt()
+    loss.backward()
+    optim.step()
 
-print(sum(vuh_res) / len(vuh_res))
-print(sum(phi_res) / len(phi_res))
+    if i % 10 == 0:
+        print(f"{i}: {loss.item()}")
+        print(frac.s.detach().item())
